6feabff030d3d079a208b36bb46029fc7da30fa4
ikramiste <ikrame.saadi@gmail.com>
2023-11-07 14:29:10 +0100

    local functional even for emails, backup this SHA case of heroku crash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    after_create :welcome_send
+    #after_create :welcome_send
     
-    has_many :events
+    has_many :attendances
     has_many :events, through: :attendances
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :description, presence: true
+    #validates :first_name, presence: true
+    #validates :last_name, presence: true
+    #validates :description, presence: true
 
-    def welcome_send
-        UserMailer.welcome_email(self).deliver_now
-    end
+    #def welcome_send
+        #UserMailer.welcome_email(self).deliver_now
+    #end
 end
