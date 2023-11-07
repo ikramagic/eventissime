3e70e8e574c6cb357df3a83128751a87cf6abf89
ikramiste <ikrame.saadi@gmail.com>
2023-11-07 16:12:24 +0100

    trying fix for sign out 2

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   #resources :users
   root 'events#index'
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
