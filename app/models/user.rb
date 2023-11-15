9abd1368be68f94effec1c90ccc4b0d85be1cbe8
ikramiste <ikrame.saadi@gmail.com>
2023-11-15 13:03:21 +0100

    reactivated mailer before push

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
-    #after_create :welcome_send
+    after_create :welcome_send
     
+    validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "You need an email address to proceed. It's mandatory." }
+
     has_many :attendances, dependent: :destroy
     has_many :events, through: :attendances, dependent: :destroy
     has_many :created_events, class_name: 'Event', foreign_key: 'admin_id'
